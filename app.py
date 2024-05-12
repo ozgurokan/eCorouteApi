806816862a8a7c18601acb49e9df834928607b9b
Özgür Okan Özdal <ozgurokanozdal@gmail.com>
2024-05-13 02:30:33 +0300

    djikstra imported, graph db changed

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bson.json_util import dumps
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from route import calculateGraph
+from route import djikstra
 
 
 
@@ -57,26 +58,30 @@ def getAllFullBinByAreaCode(areaCode):
 
 
 # areaCode a göre optimum rota döner (DJİKSTRA)
-@app.route('/api/resource/<areacode>/optimum-route', methods = ["GET"])
+@app.route('/api/resource/optimum-route/<areaCode>', methods = ["GET"])
 def getRouteByAreaCode(areaCode):
     try:
         col = db["bins"]
+        colGraph = db["graph"]
         bins_full = col.find({"bin_area_code" : int(areaCode), "is_full" : 1}) #şu an bu bölgedeki dolu kutular döndü
-        # graph = db["graphs"]
+        graph_from_db = colGraph.find_one({"graph_area_code" : int(areaCode)})
 
-        #  graphı varsa bu bölgenin graphı alınacak
-        # currentGraph = graph.find({"graph_area_code" : int(areaCode)}) #burada şu anki bölgenin dbdeki graphı döndü
+        if (graph_from_db is not None):
+            graph_for_djikstra = graph_from_db["graph"]
+            g = djikstra.Graph(len(graph_for_djikstra))
+            g.graph = graph_for_djikstra
+           
 
-        #  graphı yoksa graph hesaplanacak ama uzun sürüyor--> timeout yiyebilir?
-        # createGraph endpointi ile graphi başka yerde oluşturulsun burada var olanlar incelensin yoksa ise 404 dönsün
-
-        #  djikstra ile optimum rota hesaplanacak 
-        # json formatında optimum rota başlangıçtan sona kadar cliente gönderilecek
-        # eğer mümkünse en baştan toplam yolculuk süresi gibi veriler de yollanabilirse süper olur
+            return jsonify(graph_from_db["graph"])
+        else:
+             # createGraph endpointi ile graphi başka yerde oluşturulsun burada var olanlar incelensin yoksa ise 404 dönsün
+            return jsonify({"error" : "There is no graph information for this area"}), 404
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return jsonify({"error": "An error occurred while processing your request"}), 500
 
+
+#  graph oluşturma
 @app.route("/api/create-graph/<areaCode>", methods = ["POST"])
 def createGraphByAreaCode(areaCode):
     col = db["graph"]
@@ -87,10 +92,15 @@ def createGraphByAreaCode(areaCode):
     if(graph is None):
         binsRaw = colbins.find({"bin_area_code" : int(areaCode)})
         bins = convertBins(binsRaw)
+        ids = dict()
 
         if len(bins) > 2:
+            for i in range(len(bins)):
+                ids[str(i)] = {"index" : i, "id" : bins[i]["id"]}
+                print(ids)
             graph_created = calculateGraph.calculateGraph(bins)
-            result = col.insert_one({"graph_area_code": int(areaCode), "graph" : graph_created})
+
+            result = col.insert_one({"graph_area_code": int(areaCode), "graph" : graph_created , "id_list" : ids})
             if result.inserted_id:
                 return jsonify({"message": "Graph created succesfully"}),200
             else:
@@ -101,14 +111,32 @@ def createGraphByAreaCode(areaCode):
         return jsonify({"error" : "This graph already exists"}),409
 
 
+
+#  graph güncelleme
 @app.route("/api/update-graph/<areaCode>", methods=["UPDATE"])
 def updateGraphByAreaCode(areaCode):
-    # bölge koduna ait graph yok ise 404
-
-    # var ise aynı bölgedeki bütün noktaları alıp tekrar graph oluştur. BURASI UZUN SÜRECEK? timeout yiyebilir mi? dene ve yanıl
-    return 1
+    col = db["graph"]
+    graph = col.find_one({"graph_area_code" : int(areaCode)})
 
 
+    if(graph is not None):
+        colbins = db["bins"]
+        binsRaw = colbins.find({"bin_area_code" : int(areaCode)})
+        bins = convertBins(binsRaw)
+        if len(bins) > 2:
+            graph_created = calculateGraph.calculateGraph(bins)
+            result = col.update_one({"graph_area_code": int(areaCode), "graph" : graph_created})
+            if result.inserted_id:
+                return jsonify({"message": "Graph updated succesfully"}),200
+            else:
+                return jsonify({"error" : "Failed to update graph"}),500
+        return bin_ids
+    else:
+        return jsonify({"error" : "Graph not found"}),404
+   
+
+#  kutuyu alındı olarak işaretlendikten sonra dbde is_full'u boşaltacak endpoint 
+# rota da her sonraki dendiğinde bu endpointe update atılacak
 @app.route("/api/discharge/<binId>", methods = ["UPDATE"])
 def dischargeBinByBinId(binId):
     # bin id yok ise 404 dönsün
@@ -118,6 +146,8 @@ def dischargeBinByBinId(binId):
     # var ise çöp kutusunu bulsun ve is_full fieldını 1 ise 0 yapsın değilse bir şey yapmasın
     return 1
 
+
+# bu çöp kutusunu dolu olarak işaretlemek için gereken endpoint fakat sensörle bağlantılı olan bu değil
 @app.route("/api/fill/<binId>", methods = ["UPDATE"])
 def fillBinByBinId(binId):
     # bin id yoksa 404 dönsün
