00515b7f2e37de1f3efc0172e44df1f24671ac73
Özgür Okan Özdal <ozgurokanozdal@gmail.com>
2024-05-14 12:11:21 +0300

    endpoints change and djikstra implementation

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from route import djikstra
 
 
 
-ALLOWED_ORIGIN = 'http://127.0.0.1:5501'
+ALLOWED_ORIGIN = 'http://localhost:5173'
 
 app = Flask(__name__)
 
@@ -24,12 +24,11 @@ db = client["waste_management"]
 
 
 # areaCode a göre bütün çöp kutularını döner
-@app.route('/api/resource/<areaCode>', methods = ["GET"])
+@app.route('/api/v1/gel-all/<areaCode>', methods = ["GET"])
 def getAllBinsByAreaCode(areaCode):
     try:
         col = db["bins"]
         data = col.find({"bin_area_code" : int(areaCode)})
-
         data_list = convertBins(data)
 
         data.close()
@@ -41,12 +40,12 @@ def getAllBinsByAreaCode(areaCode):
 
 
 # area code a göre bütün dolu çöp kutularını döner
-@app.route('/api/resource/<areaCode>', methods = ["GET"])
+@app.route('/api/v1/get-all-full/<areaCode>', methods = ["GET"])
 def getAllFullBinByAreaCode(areaCode):
     try:
         col = db["bins"]
         data = col.find({"bin_area_code" : int(areaCode), "is_full" : 1})
-
+       
         data_list = convertBins(data)
 
         data.close()
@@ -58,31 +57,51 @@ def getAllFullBinByAreaCode(areaCode):
 
 
 # areaCode a göre optimum rota döner (DJİKSTRA)
-@app.route('/api/resource/optimum-route/<areaCode>', methods = ["GET"])
+@app.route('/api/v1/optimum-route/<areaCode>', methods = ["GET"])
 def getRouteByAreaCode(areaCode):
     try:
         col = db["bins"]
         colGraph = db["graph"]
-        bins_full = col.find({"bin_area_code" : int(areaCode), "is_full" : 1}) #şu an bu bölgedeki dolu kutular döndü
+        bins_full =convertBins(col.find({"bin_area_code" : int(areaCode), "is_full" : 1}))
         graph_from_db = colGraph.find_one({"graph_area_code" : int(areaCode)})
-
+        
         if (graph_from_db is not None):
             graph_for_djikstra = graph_from_db["graph"]
             g = djikstra.Graph(len(graph_for_djikstra))
             g.graph = graph_for_djikstra
-           
+            route = list()
+            spesific_nodes = [bin["index_field"] for bin in bins_full]
+            source_node = 19
+            route = []
+            while spesific_nodes:
+                distances, parent = g.dijkstra(source_node)
+                min_distance_node = min(spesific_nodes, key=lambda node: distances[node])
+                path = g.printPath(parent, min_distance_node)
+                route.extend(path)
+                spesific_nodes.remove(min_distance_node)
+                source_node = min_distance_node
 
-            return jsonify(graph_from_db["graph"])
+            unique_route = []
+            prev_node = None
+            for node in route:
+                if node != prev_node:
+                    unique_route.append(node)
+                prev_node = node
+            
+            bin_route = list()
+
+            for bin_index in unique_route:
+                temp = col.find_one({"index_field" : bin_index})
+                bin_route.append(temp)
+            return convertBins(bin_route)
         else:
-             # createGraph endpointi ile graphi başka yerde oluşturulsun burada var olanlar incelensin yoksa ise 404 dönsün
             return jsonify({"error" : "There is no graph information for this area"}), 404
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return jsonify({"error": "An error occurred while processing your request"}), 500
 
 
-#  graph oluşturma
-@app.route("/api/create-graph/<areaCode>", methods = ["POST"])
+@app.route("/api/v1/create-graph/<areaCode>", methods = ["POST"])
 def createGraphByAreaCode(areaCode):
     col = db["graph"]
     graph = col.find_one({"graph_area_code" : int(areaCode)})
@@ -96,7 +115,7 @@ def createGraphByAreaCode(areaCode):
 
         if len(bins) > 2:
             for i in range(len(bins)):
-                ids[str(i)] = {"index" : i, "id" : bins[i]["id"]}
+                ids[str(i)] = bins[i]["id"]
                 print(ids)
             graph_created = calculateGraph.calculateGraph(bins)
 
@@ -112,8 +131,7 @@ def createGraphByAreaCode(areaCode):
 
 
 
-#  graph güncelleme
-@app.route("/api/update-graph/<areaCode>", methods=["UPDATE"])
+@app.route("/api/v1/update-graph/<areaCode>", methods=["UPDATE"])
 def updateGraphByAreaCode(areaCode):
     col = db["graph"]
     graph = col.find_one({"graph_area_code" : int(areaCode)})
@@ -130,38 +148,35 @@ def updateGraphByAreaCode(areaCode):
                 return jsonify({"message": "Graph updated succesfully"}),200
             else:
                 return jsonify({"error" : "Failed to update graph"}),500
-        return bin_ids
+        return None
     else:
         return jsonify({"error" : "Graph not found"}),404
    
 
 #  kutuyu alındı olarak işaretlendikten sonra dbde is_full'u boşaltacak endpoint 
 # rota da her sonraki dendiğinde bu endpointe update atılacak
-@app.route("/api/discharge/<binId>", methods = ["UPDATE"])
+@app.route("/api/v1/discharge/<binId>", methods = ["UPDATE"])
 def dischargeBinByBinId(binId):
-    # bin id yok ise 404 dönsün
-
-    # db.collectionName.findById('objectIdHere') -- id ile döndürme
-
-    # var ise çöp kutusunu bulsun ve is_full fieldını 1 ise 0 yapsın değilse bir şey yapmasın
-    return 1
-
+    col = db["bins"]
+    bin = col.find_one({"_id" : str(binId)})
+    if bin:
+        col.update_one({"_id" : str(binId)} , {"is_full" : 0})
+        return jsonify({"success" : "bin discharge succesfully"}),200
+    else:
+        return jsonify({"error" : "bin not found id" + str(binId)}),404
 
 # bu çöp kutusunu dolu olarak işaretlemek için gereken endpoint fakat sensörle bağlantılı olan bu değil
-@app.route("/api/fill/<binId>", methods = ["UPDATE"])
+@app.route("/api/v1/fill/<binId>", methods = ["UPDATE"])
 def fillBinByBinId(binId):
-    # bin id yoksa 404 dönsün
+    col = db["bins"]
+    bin = col.find_one({"_id" : str(binId)})
+    if bin:
+        col.update_one({"_id" : str(binId)} , {"is_full" : 1})
+        return jsonify({"success" : "bin fill succesfully"}),200
+    else:
+        return jsonify({"error" : "bin not found id" + str(binId)}),404
 
-    
-    # db.collectionName.findById('objectIdHere') -- id ile döndürme
-    
-    # var ve is_full 0 ise 1 yapsın değilse bir şey yapmasın
-    return 1
 
-
-
-def exportData():
-    return client
 
 def convertBins(data):
     data_list = []
@@ -169,6 +184,7 @@ def convertBins(data):
     for item in data:
         data_dict = {
             "id" : str(item["_id"]),
+            "index_field" : int(item["index_field"]),
             "bin_lng" : item["bin_lng"],
             "bin_lat" : item["bin_lat"],
             "bin_location_str" : item["bin_location_str"],
